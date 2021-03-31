1c78a662bb0079713513c321222b45b3cc8b461e
Rahulrck <80900027+Rahulrck@users.noreply.github.com>
2021-04-01 00:28:00 +0530

    Add files via upload

---
+++ p12.php
@@ -0,0 +1,64 @@
+<?php
+$ready=readline("are you ready for a quiz(yes/no) :");
+echo $ready;
+echo ("\n");
+
+if(ready==yes){
+echo "oh k  here it comes";
+echo ("\n");
+}
+$cap=readline("what is the capital of alask(please enter)");
+echo ("\n");
+echo "1)melborne";
+echo ("\n");
+echo "2)anchorage";
+echo ("\n");
+echo "3)juneau";
+echo "cap";
+
+$num = readline("enter the ans :");
+if ($num==3){
+echo "correct";
+}
+else{
+echo "not correct";
+}
+echo("\n");
+
+$ca=readline("can you store the value 'cat' in a varible of type int(please enter)");
+echo ("\n");
+echo "1)yes";
+echo ("\n");
+echo "2)no";
+echo ("\n");
+echo "ca";
+
+$num = readline("enter the ans :");
+if ($ca==2){
+echo "correct";
+}
+else{
+echo "not correct";
+}
+$c=readline("what is result of 9+6/3 ?(please enter)");
+echo ("\n");
+echo "1)3";
+echo ("\n");
+echo "2)13";
+echo ("\n");
+echo "3)21";
+echo ("\n");
+echo "c";
+
+$nu = readline("enter the ans :");
+if ($nu==1){
+echo "correct";
+}
+else{
+echo "not correct";
+}
+
+  
+
+
+?>
--- p14.php
+++ p14.php
@@ -1,9 +1,42 @@
 <?php
+$a= readline("WELCOME TO MITCHELL'S TINY ADVENTURE!");
+echo "$a";
+echo ("\n");
+	echo "You are in a creepy house! Would you like to go upstairs or into the kitchen ?(kitchen/upstairs)\n";
+	$select=readline('');
+	if($select==='kitchen')
+	{
+		echo "\n\nThere is a long countertop with dirty dishes everywhere. off to one side\n";
+		echo "there is, as you'd except, a refrigerator. You may open the refrigerator\n";
+		echo "or look in a cabinet(refrigerator / cabinet).\n";
+		$second=readline('');		
+		if($second==='refrigerator')
+		{
+			echo "\n\nInside the refrigerator you see food and stuff. It looks pretty nasty.\n";
+			echo "Would you like to eat some of the food?(yes / no)\n";
+			$final=readline('');
+			if($final==='yes')
+			{
+				echo "\n\nOkay have your Food!!!\n";
+			}
+			else
+			{
+				echo "\n\nYou die of starvation...  eventually.\n";
+			}	
+		}
+		else
+		{
+			echo "\n\nInside the Cabinet only have Books and News papers.\n";
+		}
+	}
+	else
+	{
+		echo "\n\nUpstairs rooms are so dirty you go to the kitchen.";
+	}
 
-$a="WELCOME TO MITCHELL'S TINY ADVANTURE";
-echo $a;
-echo ("\n");
-$b=readline("you'r in a creepy house!would you like to go 'upstair' into the kitchen ?");
-echo ("\n");
-echo $b;
-$c=readline("there is a long countertop with dirty dishes everywhere .off to one side there is as you except a "refrigerato" or look in a "cabnet.");
+
+
+
+
+
+?>
